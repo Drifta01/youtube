cf6482cb6998c58324f7e60d7687b801ea6f358c
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-06-01 11:13:45 +1200

    Add a paste button

--- downloader/gui.py
+++ downloader/gui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from .yt import download_yt_video
+
 
 def create_gui():
     root = tk.Tk()
@@ -11,9 +13,17 @@ def create_gui():
     entry = tk.Entry(root)
     entry.pack()
 
-    button = tk.Button(root, text="Download")
-    button.pack()
+    def paste_from_clipboard():
+        entry.insert(tk.END, root.clipboard_get())
 
-    root.children = {"entry": entry, "button": button}
+    entry.bind("<Control-v>", lambda event: paste_from_clipboard())
+
+    button_paste = tk.Button(root, text="Paste URL", command=paste_from_clipboard)
+    button_paste.pack()
+
+    button_dl = tk.Button(
+        root, text="Download", command=lambda: download_yt_video(entry.get())
+    )
+    button_dl.pack()
 
     return root
--- downloader/main.py
+++ downloader/main.py
@@ -1,12 +1,8 @@
 from .gui import create_gui
-from .yt import download_yt_video
 
 
 def main():
     root = create_gui()
-    entry = root.children["entry"]
-    button = root.children["button"]
-    button.config(command=lambda: download_yt_video(entry.get()))
     root.mainloop()
 
 
