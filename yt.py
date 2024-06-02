e32bf9a314a2a37a166ea62db710e91153054250
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-06-02 21:24:18 +1200

    Download to ~/Downloads

--- yt.py
+++ yt.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import messagebox
 from pytube import YouTube
 
@@ -8,11 +9,15 @@ def download_yt_video(url):
     try:
         yt.check_availability()
         streams = yt.streams
-        video = streams.filter(res="720p").first()
+        video = streams.filter(res="480p").first()
+
         print(f"Downloading {video.title}")
-        video.download(output_path=".", skip_existing=True)
+        downloads_folder = os.path.expanduser("~/Downloads")
+        video.download(output_path=downloads_folder, skip_existing=True)
+
         print("Download complete!")
         messagebox.showinfo("Success", "Download completed successfully!")
+
     except Exception as e:
         print(e)
         messagebox.showerror("Error", str(e))
